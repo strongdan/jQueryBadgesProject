7253361d96792dac3294053d4c54aeb7c05fa09f
Dan Strong <danielstrong@mac.com>
2017-07-06 11:17:32 -0800

    Update main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,40 @@
 $(function() {
+  $.ajax({
+      url: 'https://www.codeschool.com/users/strongdan.json',
+      dataType: 'jsonp',
+      success: function(response) {
+        addCourses(response.courses.completed);
+      }
+    });
 
-  // your code will go here
+    function addCourses(courses) {
+      var $badges = $('#badges');
+      courses.forEach(function(course) {
+        var $course = $('<div />', {
+          'class': 'course'
+        }).appendTo($badges);
 
+        $('<h3 />', {
+          text: course.title
+        }).appendTo($course);
+
+        $('<img />', {
+          src: source.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($course);
+      
+      })
+      
+    }
+
+    
+    // add one div element per item in the courses.completed array inside of the #badges element. 
+    // Add the CSS class course to each div element we add to #badges.	MENU
 });
+
